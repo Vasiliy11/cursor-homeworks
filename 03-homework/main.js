828e84bae4dd7e96cd4d3a025fd6721094b9aba6
Vasiliy11 <vasiliy13410@gmail.com>
2020-11-21 14:28:24 +0200

    add 2 functions

--- 03-homework/main.js
+++ 03-homework/main.js
@@ -74,6 +74,37 @@ const countLetter = function (letter, str) {
   return count;
 };
 
+// Конвертер валют
+const exchangeCurrency =  function (param) {
+  const uahTo$ = 1/28;
+  const $toUAH = 28;
+  result = null;
+  do {
+      param = prompt('Введіть валюту для конвертації');
+  } while ((param.indexOf('uah') === -1) && (param.indexOf('$') === -1) || isNaN(parseFloat(param)))
+  if(param.includes('uah')) {
+      result = parseFloat(param)*uahTo$; 
+  } else if (param.includes('$')) {
+      result = parseFloat(param)*$toUAH
+  }
+  return result
+}
+
+// Генератор пароля
+
+const getRandomPassword = function (num) {
+  do {
+    num = +prompt('Введіть число для генерації пароля', 8);
+  } while (!Number.isInteger(num));
+	let password = '';
+	if (num === 0) {
+    num = 8;
+  }
+	for (let i = 1; i <= num; i++){
+		password += Math.floor(Math.random()*10);
+	}
+	return password;
+}
 
 // Паліндром
 const isPalindrom = function (str) {
@@ -86,9 +117,11 @@ const isPalindrom = function (str) {
 
 
 document.writeln(`Функція №1: максимальна цифра: ${getMaxDigit()} <br>
-Функція №: число пднесене до степеня: ${powerOfNumber()} <br>
+Функція №2: число пднесене до степеня: ${powerOfNumber()} <br>
 Функція №3: правильно написане ім\'я: ${formatName()} <br>
 Функція №4: чиста зарплата: ${calculateSalary()} <br>
 Функція №5: рандомне число в заданому діапазоні: ${getRandomNumber()} <br>
 Функція №6: кількість літер у реченні: ${countLetter()} <br>
+Функція №7: конвертована валюта: ${exchangeCurrency()} <br>
+Функція №8: ваш пароль: ${getRandomPassword()} <br>
 Функція №10: чи є фраза паліндромом?  - ${isPalindrom()}`);
