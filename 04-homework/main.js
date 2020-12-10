5016a9c26e5fb7710405c3bc12cd80484497a472
Vasiliy11 <vasiliy13410@gmail.com>
2020-12-11 00:06:55 +0200

    fix something

--- 04-homework/main.js
+++ 04-homework/main.js
@@ -1,16 +1,12 @@
 const students = ['Олександр', 'Ігор', 'Олена', 'Іра', 'Олексій', 'Світлана'];
-const themes = [
-  'Диференційне рівняння',
-  'Теорія автоматів',
-  'Алгоритми і структури даних',
-];
+const themes = ['Диференційне рівняння', 'Теорія автоматів','Алгоритми і структури даних'];
 const marks = [4, 5, 5, 3, 4, 5];
 
 // Завдання 1
 const pairStudents = (arr) => {
   const pairs = [];
   for (let i = 0; i < arr.length; i += 2) {
-    pairs.push(arr.slice(i, i + 2));
+    pairs.push([students[i], students[i + 1]]);
   }
   return pairs;
 };
@@ -18,23 +14,23 @@ const pairStudents = (arr) => {
 const pairsOfStudents = pairStudents(students);
 
 // Завдання 2
-const learnTheme = function (arrTheme, arrPairs) {
+const learnTheme = (arrTheme, arrPairs) => {
   const group = [];
-  const newPairs = arrPairs.slice();
-  for (let i = 0; i < arrTheme.length; i++) {
-    group.push([newPairs[i].join(' i '), arrTheme[i]]);
-  }
+  const newPairs = [...arrPairs];
+  newPairs.forEach((pair, index) => {
+    group[index] = [pair.join(' і '), arrTheme[index]];
+  });
   return group;
 };
 
 const groups = learnTheme(themes, pairsOfStudents);
 
 // Завдання 3
-const rateStudents = function (arrStudents, arrMarks) {
+const rateStudents = (arrStudents, arrMarks) => {
   const studentMark = [];
-  for (let i = 0; i < arrStudents.length; i++) {
-    studentMark.push([arrStudents[i], arrMarks[i]]);
-  }
+  arrStudents.forEach(
+    (student, index) => (studentMark[index] = [student, arrMarks[index]])
+  );
   return studentMark;
 };
 
@@ -43,13 +39,14 @@ const studentsMarks = rateStudents(students, marks);
 // Завдання 4
 const rateGroups = function (arrPairs, themes) {
   const projectMark = [];
-  for (i = 0; i < arrPairs.length; i++) {
-    projectMark.push([
-      arrPairs[i].join(' , '),
-      themes[i],
-      Math.floor(Math.random() * 5) + 1,
-    ]);
-  }
+  arrPairs.forEach(
+    (pair, index) =>
+      (projectMark[index] = [
+        pair.join(' і '),
+        themes[index],
+        Math.floor(Math.random() * 5) + 1,
+      ])
+  );
   return projectMark;
 };
 
