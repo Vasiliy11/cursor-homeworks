adedff118860123102ee18435fa1ae3a87d90345
Vasiliy11 <vasiliy13410@gmail.com>
2020-12-15 19:08:00 +0200

    start hw

---
+++ 10-homework/js/main.js
@@ -0,0 +1,9 @@
+const keys = document.querySelector('.keys');
+const keypress = document.querySelector('.key');
+const intBtn = document.querySelector('.create-interval-blocks');
+
+function playSound() {
+  return;
+}
+
+keypress.addEventListener('keydown', playSound);
